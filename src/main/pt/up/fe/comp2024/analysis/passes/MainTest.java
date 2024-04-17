8d301de60ad85b461f0f921ab426975ccbb893c9
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2024-04-17 13:30:21 +0100

    fix bugz

--- src/main/pt/up/fe/comp2024/analysis/passes/MainTest.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/MainTest.java
@@ -19,20 +19,21 @@ public class MainTest extends AnalysisVisitor {
     private Void visitMethodDecl(JmmNode method, SymbolTable table) {
         currentMethod = method.get("name");
 
-        var isStatic = method.get("isStatic").equals("true");
-        if (!isStatic) {
-            String message = "Method main must be static";
-            addReport(Report.newError(
-                    Stage.SEMANTIC,
-                    NodeUtils.getLine(method),
-                    NodeUtils.getColumn(method),
-                    message,
-                    null)
-            );
-            return null;
-        }
-
         if (currentMethod.equals("main")) {
+
+            var isStatic = method.get("isStatic").equals("true");
+            if (!isStatic) {
+                String message = "Method main must be static";
+                addReport(Report.newError(
+                        Stage.SEMANTIC,
+                        NodeUtils.getLine(method),
+                        NodeUtils.getColumn(method),
+                        message,
+                        null)
+                );
+                return null;
+            }
+
             var mainChild = method.getChild(1);
             if (mainChild.getKind().equals("ReturnStmt")) {
                 String message = "Main must be void";
@@ -62,8 +63,8 @@ public class MainTest extends AnalysisVisitor {
             return null;
 
         } else {
-            var isStatic2 = method.get("isStatic").equals("true");
-            if (isStatic2) {
+            var isStatic = method.get("isStatic").equals("true");
+            if (isStatic) {
                 String message = "Method cannot be static";
                 addReport(Report.newError(
                         Stage.SEMANTIC,
