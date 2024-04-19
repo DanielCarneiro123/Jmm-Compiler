d7df341c43ec04db9c7d471164196e2900aa2c4d
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2024-04-19 10:51:24 +0100

    crl

--- src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredMethod.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredMethod.java
@@ -1,12 +1,15 @@
 package pt.up.fe.comp2024.analysis.passes;
 
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
+import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.comp2024.analysis.AnalysisVisitor;
 import pt.up.fe.comp2024.ast.Kind;
 import pt.up.fe.comp2024.ast.NodeUtils;
+
+import static pt.up.fe.comp2024.ast.TypeUtils.getExprType;
 
 public class UndeclaredMethod extends AnalysisVisitor {
     private String method;
@@ -17,6 +20,7 @@ public class UndeclaredMethod extends AnalysisVisitor {
         addVisit(Kind.METHOD_DECL, this::visitMethodDecl);
         addVisit(Kind.NEW_CLASS, this::visitUndeclaredMethod);
         addVisit(Kind.CLASS_DECLARATION, this::visitImport_Extend);
+        addVisit(Kind.FUNCTION_CALL, this::visitFunctionCaller);
     }
 
     private Void visitMethodDecl(JmmNode currMethod, SymbolTable table) {
@@ -57,6 +61,31 @@ public class UndeclaredMethod extends AnalysisVisitor {
                     return null;
                 }
             }
+        }
+
+        return null;
+    }
+
+    private Void visitFunctionCaller(JmmNode functionCall, SymbolTable table) {
+        var functionCallChild = functionCall.getChild(0);
+        var functionCallChildName = functionCallChild.getOptional("name").orElse("");
+        var functionCallChildType = new Type("", false);
+        if (!functionCallChildName.equals("this")) {
+            functionCallChildType = getExprType(functionCallChild, table, method);
+        } else {
+            return null;
+        }
+
+        if (functionCallChildName.equals("int") || functionCallChildName.equals("boolean") || functionCallChildType.isArray()) {
+            String message = "Wrong Type Caller";
+            addReport(Report.newError(
+                    Stage.SEMANTIC,
+                    NodeUtils.getLine(functionCall),
+                    NodeUtils.getColumn(functionCall),
+                    message,
+                    null)
+            );
+            return null;
         }
 
         return null;
