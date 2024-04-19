b75f7d12bc3d941188311a1147fca4eac75ca6fc
DanielCarneiro123 <danielmc2116@gmail.com>
2024-04-19 11:31:28 +0100

    semantic updates

--- src/main/pt/up/fe/comp2024/ast/TypeUtils.java
+++ src/main/pt/up/fe/comp2024/ast/TypeUtils.java
@@ -49,7 +49,7 @@ public class TypeUtils {
 
         Type type = switch (kind) {
             case IDENTIFIER, VAR_DECL, INT, ID -> getVarExprType(expr, table, currMethod);
-            case ARRAYDEFINITION, ARRAY_DECLARATION -> new Type(INT_TYPE_NAME, true);
+            case ARRAYDEFINITION, ARRAY_DECLARATION, ARRAY_SUBSCRIPT -> new Type(INT_TYPE_NAME, true);
             case INTEGER, CLASS_INSTANTIATION -> new Type(INT_TYPE_NAME, false);
             case IFEXPR, ELSEEXPR -> new Type(BOOLEAN_TYPE_NAME, false);
             case BINARY_EXPR -> getBinExprType(expr);
