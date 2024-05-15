738ca1266c11910a730d9e7665c852b13b8988d3
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2024-05-15 12:45:48 +0100

    .

--- src/main/pt/up/fe/comp2024/ast/TypeUtils.java
+++ src/main/pt/up/fe/comp2024/ast/TypeUtils.java
@@ -50,8 +50,8 @@ public class TypeUtils {
 
         Type type = switch (kind) {
             case IDENTIFIER, VAR_DECL, INT, ID -> getVarExprType(expr, table, currMethod);
-            case ARRAYDEFINITION, ARRAY_DECLARATION -> new Type(INT_TYPE_NAME, true);
-            case INTEGER, CLASS_INSTANTIATION, ARRAY_SUBSCRIPT -> new Type(INT_TYPE_NAME, false);
+            case ARRAYDEFINITION, ARRAY_DECLARATION, ARRAY_SUBSCRIPT -> new Type(INT_TYPE_NAME, true);
+            case INTEGER, CLASS_INSTANTIATION -> new Type(INT_TYPE_NAME, false);
             case IFEXPR, ELSEEXPR -> new Type(BOOLEAN_TYPE_NAME, false);
             case BINARY_EXPR -> getBinExprType(expr);
             case BINARY_OP -> getBinExprType(expr);
