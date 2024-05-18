b4cf7dab290971df5b1d23c6dad642fc73b9aac9
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2024-05-18 21:03:00 +0100

    .

--- src/main/pt/up/fe/comp2024/optimization/OptUtils.java
+++ src/main/pt/up/fe/comp2024/optimization/OptUtils.java
@@ -1,15 +1,6 @@
 package pt.up.fe.comp2024.optimization;
 
-import org.specs.comp.ollir.Instruction;
-import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.JmmNode;
-import pt.up.fe.comp2024.ast.NodeUtils;
-import pt.up.fe.specs.util.exceptions.NotImplementedException;
-
-import java.util.List;
-import java.util.Optional;
-
-import static pt.up.fe.comp2024.ast.Kind.TYPE;
 
 public class OptUtils {
     private static int tempNumber = -1;
@@ -39,10 +30,10 @@ public class OptUtils {
         return toOllirType(typeName);
     }
 
-    public static String toOllirType(Type type) {
+    /*public static String toOllirType(Type type) {
         if(type.isArray()) return ".array" + toOllirType(type.getName());
         return toOllirType(type.getName());
-    }
+    }*/
 
     private static String toOllirType(String typeName) {
 
