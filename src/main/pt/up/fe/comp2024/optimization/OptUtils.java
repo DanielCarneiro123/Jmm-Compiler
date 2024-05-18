d8862a3a6ddfc468f5f60694ba3bc73ad0ad5a8a
koaLaMoucho <koalamoucho@gmail.com>
2024-05-18 23:28:54 +0100

    checking if toollirtype influences syntax

--- src/main/pt/up/fe/comp2024/optimization/OptUtils.java
+++ src/main/pt/up/fe/comp2024/optimization/OptUtils.java
@@ -32,7 +32,7 @@ public class OptUtils {
     }
 
     public static String toOllirType(Type type) {
-        if(type.isArray()) return ".array" + toOllirType(type.getName());
+
         return toOllirType(type.getName());
     }
 
